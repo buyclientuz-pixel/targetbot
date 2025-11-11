4c954454e2a3377e48627aafed9cbc2c41b1b908
buyclientuz-pixel <buyclientuz@gmail.com>
2025-11-12 03:16:18 +0500

    chore: add flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    ignores: ['dist/**', '.wrangler/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      quotes: ['error', 'backtick'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      semi: ['error', 'always']
+    }
+  }
+];
