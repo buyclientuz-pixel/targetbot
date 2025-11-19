b7754371c00a191d735f1c11dc11d9f2ff3ef5be
buyclientuz-pixel <buyclientuz@gmail.com>
2025-11-20 00:09:49 +0500

    Add formatting and secrets scripts and expand checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,38 @@
 import prettierPlugin from "eslint-plugin-prettier";
+import tseslint from "typescript-eslint";
+
+const commonRules = {
+  "prettier/prettier": "error",
+  quotes: ["error", "double", { avoidEscape: true }],
+  "no-var": "error",
+  "prefer-const": "error",
+  semi: ["error", "always"],
+};
+
+const commonIgnores = ["dist/**", ".wrangler/**", "node_modules/**"];
 
 export default [
+  ...tseslint.config({
+    files: ["**/*.{ts,tsx}"],
+    ignores: commonIgnores,
+    extends: [...tseslint.configs.recommended],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: commonRules,
+  }),
   {
     files: ["**/*.js"],
-    ignores: ["dist/**", ".wrangler/**", "node_modules/**"],
+    ignores: commonIgnores,
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -14,12 +43,6 @@ export default [
     plugins: {
       prettier: prettierPlugin,
     },
-    rules: {
-      "prettier/prettier": "error",
-      quotes: ["error", "double", { avoidEscape: true }],
-      "no-var": "error",
-      "prefer-const": "error",
-      semi: ["error", "always"],
-    },
+    rules: commonRules,
   },
 ];
